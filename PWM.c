c7b77359f85cced51c04b632a6f38f5b77bb27bb
Dulce-Tapia <113047668+Dulce-Tapia@users.noreply.github.com>
2023-01-24 16:34:48 -0600

    Actualización de la Practica_5TM4C129B
    Se corrigió un error de doble comilla y agregó evidencia de frecuencia de 50 Hz

--- PWM.c
+++ PWM.c
@@ -15,12 +15,12 @@ extern void Configura_Reg_PWM0(uint16_t freq)
     //Indicar funcion alternativa (PWM) (PCTL) p. 689, 1808
     //6 indica que es PWM
     GPIOG_AHB->PCTL |= (GPIOG_AHB->PCTL&0xFFFFFFF0) | 0x00000006;
-    GPIOF_AHB->PCTL |= (GPIOG_AHB->PCTL&0xFFFFF00F) | 0x00000660;
+    GPIOF_AHB->PCTL |= (GPIOF_AHB->PCTL&0xFFFFF00F) | 0x00000660;
     //pin digital pag 682
     GPIOG_AHB->DEN |= (1<<0); 
     GPIOF_AHB->DEN |= (1<<1)|(1<<2);
     //habilitar divisor (RCC) p. 254 Enable o Disable Divisor  Pag 1747
-    PWM0->CC = (1<<8)|(0<<2)|(1<<1)|(0<<0);;    
+    PWM0->CC = (1<<8)|(0<<2)|(1<<1)|(0<<0);    
     //deshabilitar generador (CTL) p. 1266, 1233
     PWM0->_2_CTL = (0<<0);
     PWM0->_0_CTL = (0<<0);
@@ -36,7 +36,7 @@ extern void Configura_Reg_PWM0(uint16_t freq)
     PWM0->_0_LOAD = 50000;
     PWM0->_2_CMPA = 35000;     //Ciclo de trabajo Nota *Regla de 3
     PWM0->_1_CMPA = 35000;
-    PWM0->_0_CMPA = 35000;
+    PWM0->_0_CMPB = 35000;
     // Se activa el generador 0,1,2
     PWM0->_2_CTL = (1<<0);
     PWM0->_1_CTL = (1<<0);
